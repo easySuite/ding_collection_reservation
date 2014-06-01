1b361e62be809a4346d64314ad9dd52ad464e64c
Max Melnic <mm@inleadmedia.com>
2014-06-01 21:04:34 +0300

    Case 4175 - show reservation buttons on popup open.

--- js/ding_collection_reservation.js
+++ js/ding_collection_reservation.js
@@ -3,16 +3,8 @@
  */
 (function($) {
   $(document).on('dialogopen', function(event, ui) {
-    var reserve_buttons_ids = [];
-
     $('.ding-popup-content .reserve-button').each(function() {
-      reserve_buttons_ids.push($(this).attr('id').split(':'));
-    });
-
-    $.each(reserve_buttons_ids, function(id, content_id) {
-      if (Drupal.DADB[content_id[1]].reservable) {
-        $('.field-type-ding-entity-buttons a.reserve-button[id$="' + content_id[1] + '"]').addClass('reservable');
-      }
+      $(this).show();
     });
   });
 })(jQuery);
