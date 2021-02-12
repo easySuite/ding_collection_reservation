9677469c712c89e37d7943a5bf4e7dececc1a567
Alex <bozeak@gmail.com>
2021-02-12 13:43:25 +0200

    EASYOPAC-1271 - Check collection's reserveability and render button.

--- js/ding_collection_reservation.js
+++ js/ding_collection_reservation.js
@@ -1,21 +1,77 @@
 /**
  * @file Handle show of reserve buttons in popup
  */
-(function($) {
+(function ($) {
   'use strict';
   Drupal.behaviors.ding_collection_reservation = {
-    attach: function(context, settings) {
-      let observer = new MutationObserver(function(mutations) {
+    attach: function (context) {
+      // Detect collection's reserveability.
+      let found = [];
+      let selector = '.js-check-collection-reservability';
+      // Collect collection object id's from attributes.
+      $(selector, context).once('js-check-collection-reservability', function () {
+        let $this = this;
+        let items = $($this, selector).parent().parent().parent().parent().parent().attr('ting-collection-items');
+        if (items) {
+          found.push(items);
+        }
+      });
+
+      // Clean strings, transform and push for processing.
+      let dataForProcessing = [];
+      $.each(found, function (index, value) {
+        // Remove unwanted "[" and "]" characters and transform string to array.
+        let dataArray = value.substring(1, value.length - 1).split(',');
+
+        let cleaned = [];
+        // Remove quotes wrapping the array values.
+        $.each(dataArray, function (i, v) {
+          let cleanVal = v.substring(1, v.length - 1);
+          cleaned.push(cleanVal);
+        });
+
+        dataForProcessing.push({
+          'primary': cleaned[0],
+          'data': cleaned
+        });
+      });
+
+      if (dataForProcessing.length) {
+        dataForProcessing.forEach(function (collection) {
+          $.ajax({
+            dataType: 'json',
+            type: 'POST',
+            url: '/ding_reservation/is_reservable',
+            data: {
+              localIds: collection.data
+            },
+            success: function (result) {
+              let resultData = [];
+              $.each(result, function (localId, reservable) {
+                if (reservable) {
+                  resultData.push(reservable);
+                }
+              });
+
+              if (resultData.length > 0) {
+                $(selector + '[data-local-id="' + collection.primary + '"]', context).addClass('reservable');
+              }
+            }
+          });
+        });
+      }
+
+      let observer = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
           if (mutation.attributeName === "class" && $(mutation.target).hasClass('popupbar-is-open')) {
-            $('#popupbar-collection_reservation').find('.reserve-button').each(function() {
-              let local_id = $(this).attr('id').split(':')[1];
+            $('#popupbar-collection_reservation').find('.reserve-button').each(function () {
+              let local_id = $(this).attr('data-local-id');
               if (Drupal.DADB[local_id].reservable) {
                 $(this).addClass('reservable');
               }
             });
           }
-        })
+        });
       });
 
       let $body = $("body", context);
